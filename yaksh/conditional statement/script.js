2408dc21afc31447ceb8f428c07dd03bf540a243
yakshpatel11 <yakshp1112@gmail.com>
2025-04-23 11:01:47 +0530

    Add files via upload

---
+++ yaksh/conditional statement/script.js
@@ -0,0 +1,15 @@
+//Voting Eligibilty.//
+let age =19;
+if (age >18) {document.writeln ("Eligible For Vote !")
+}
+if (age <18) {document.writeln ("Not Eligible For Vote !")
+}
+else if (age <=0) {document.writeln ("Not Valid Age !")
+    
+}
+ 
+
+// didvisable By 7 Or Not //
+let number=49; 
+if (number==0) {document.writeln("number is even")
+}
